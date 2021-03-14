c85ab70a2cb7c40738ec6487ee61b41d434f7681
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-03-14 17:50:19 +0500

    solution to sum-of-positives

--- codewars/8-kyu/sum-of-positives.js
+++ codewars/8-kyu/sum-of-positives.js
@@ -6,9 +6,15 @@ You get an array of numbers, return the sum of all the positive ones. if the arr
 
 */
 
-function SumPositiveValues(arr) {
+function sumPositiveValues(arr) {
   // Return 0 if the array is empty
   if (arr.length === 0) return 0;
 
   // Filter out all the positive values and return their sum
+  return arr.filter((num) => Math.sign(num) === 1).reduce((acc, currentValue) => acc + currentValue, 0);
 }
+
+console.log(sumPositiveValues([1, 2, 3, 4, 5])); // Should Return 15
+console.log(sumPositiveValues([1, -2, 3, 4, 5])); // Should Return 13
+console.log(sumPositiveValues([])); // Should Return 0
+console.log(sumPositiveValues([-1, -2, -3, -4, -5])); // Should Return 0
